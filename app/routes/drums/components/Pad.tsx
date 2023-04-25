2af0ac04cb6a7541aa1bd9221a0390e67a52b427
Andrei Khamedov <xamedow@gmail.com>
2023-04-25 21:12:49 +0400

    Move audio sample loading and playing logic inside pad component

--- app/routes/drums/components/Pad.tsx
+++ app/routes/drums/components/Pad.tsx
@@ -1,40 +1,103 @@
-import { useState } from "react";
-import type { ChangeEventHandler, FC } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import type { ChangeEventHandler, MouseEventHandler, FC } from "react";
 
 type PadProps = {
-  soundName: string,
-  onMouseDown: () => void,
-  defaultGainValue: number,
-  onGainChange: (value: number) => void,
+  audioParams: { key: string, path: string },
+  audioContext?: AudioContext,
+  audioTitle: string,
 }
 
+const DEFAULT_GAIN_VALUE = 1;
+
 const Pad: FC<PadProps> = ({
-  soundName,
-  onMouseDown,
-  defaultGainValue,
-  onGainChange,
+  audioParams,
+  audioContext,
+  audioTitle,
 }) => {
-  const [value, setValue] = useState<number>(defaultGainValue);
+  const audioBuffer = useRef<AudioBuffer>();
+  const [loading, setLoading] = useState(false);
+  const [gainValue, setGainValue] = useState<number>(DEFAULT_GAIN_VALUE);
+
+  const prepareSound = useCallback(async () => {
+    if (audioContext) { // Re-use the audio buffer as a source
+      const response = await fetch(audioParams.path);
+
+      // Turn into an array buffer of raw binary data
+      const arrayBuffer = await response.arrayBuffer();
+
+      // Decode the entire binary MP3 into an AudioBuffer
+      return await audioContext.decodeAudioData(arrayBuffer);
+    }
+  }, [audioContext, audioParams.path]);
+
+
+  const playSound = useCallback(() => {
+    if (audioContext && audioBuffer.current) {
+      // Now create a new "Buffer Source" node for playing AudioBuffers
+      const source = audioContext.createBufferSource();
+
+      // Initialising gain
+      const gainNode = audioContext.createGain();
+      source.connect(gainNode);
+
+      // Changing gain value according to sound pad's control
+      gainNode.gain.value = gainValue;
+
+      // Here we need to connect gain to destination, but not the source,
+      // so the flow would be [Buffer] -> [GainNode] -> [Destination]
+      gainNode.connect(audioContext.destination);
+
+      // Assign the loaded buffer
+      source.buffer = audioBuffer.current;
+
+      // Start (zero = play immediately)
+      source.start(0);
+    }
+  }, [audioContext, gainValue]);
+
+  const handleKeyboardEvents = ({ key }: KeyboardEvent) => {
+    if (key == audioParams.key) {
+      playSound();
+    }
+  };
+
+  const handlePlayMouseDown: MouseEventHandler = () => playSound();
 
   const handleGainChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setValue(Number(e.currentTarget.value));
-    onGainChange(Number(e.currentTarget.value));
+    setGainValue(Number(e.currentTarget.value));
   };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboardEvents);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboardEvents);
+    };
+  });
+
+  useEffect(() => {
+    setLoading(true)
+    prepareSound().then((decodedAudioBuffer?: AudioBuffer) => {
+      audioBuffer.current = decodedAudioBuffer;
+      setLoading(false);
+    }).catch(console.error);
+  }, [prepareSound]);
 
 
   return <div>
     <button
+      disabled={loading}
       className="bg-white shadow w-24 h-24 lg:w-64 lg:h-64 rounded"
-      onMouseDown={onMouseDown}
+      onMouseDown={handlePlayMouseDown}
     >
-      {soundName}
+      {audioTitle}{loading && ':Loading'}
     </button>
     <input
       type="range"
       min="0"
       max="1"
       step="0.01"
-      value={value}
+      value={gainValue}
       onChange={handleGainChange}
     />
   </div>;
--- app/routes/drums/index.tsx
+++ app/routes/drums/index.tsx
@@ -1,11 +1,6 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import type { ReactNode } from "react";
 import Pad from "./components/Pad";
-
-type BufferType = {
-  buffer: AudioBuffer;
-  gainValue: number;
-}
 
 // Discuss: little quirks implementation
 const DRUMKIT_PRESET = new Map([
@@ -21,86 +16,17 @@ const DRUMKIT_PRESET = new Map([
 
 
 export default function Drums() {
-  const [loading, setLoading] = useState(false);
-  const audioContext = useRef<AudioContext>();
-  const audioBuffer = useRef<Record<string, BufferType>>({});
-
-  const loadSound = useCallback(async (soundName: string, { path: soundPath }: Record<string, string>) => {
-    // Re-use the same context if it exists
-    if (!audioContext.current) {
-      audioContext.current = new AudioContext();
-    }
-
-    // Re-use the audio buffer as a source
-    // Fetch MP3 from URL
-    const resp = await fetch(soundPath);
-
-    // Turn into an array buffer of raw binary data
-    const buf = await resp.arrayBuffer();
-
-    // Decode the entire binary MP3 into an AudioBuffer
-    const buffer = await audioContext.current.decodeAudioData(buf);
-    const gainValue = 1;
-
-    audioBuffer.current[soundName] = { buffer, gainValue };
-  }, []);
-
-  const loadSounds = useCallback(async () => {
-    for (const [soundName, soundProperty] of DRUMKIT_PRESET) {
-      await loadSound(soundName, soundProperty);
-    }
-  }, [loadSound]);
-
-  const playSound = (soundName: string) => {
-    if (audioContext?.current) {
-      // Now create a new "Buffer Source" node for playing AudioBuffers
-      const source = audioContext.current.createBufferSource();
-
-      // Initialising gain
-      const gainNode = audioContext.current.createGain();
-      source.connect(gainNode);
-
-      // Changing gain value according to sound pad's control
-      gainNode.gain.value = audioBuffer.current[soundName].gainValue;
-
-      // Here we need to connect gain to destination, but not the source,
-      // so the flow would be [Buffer] -> [GainNode] -> [Destination]
-      gainNode.connect(audioContext.current.destination);
-
-      // Assign the loaded buffer
-      source.buffer = audioBuffer.current[soundName].buffer;
-
-      // Start (zero = play immediately)
-      source.start(0);
-    }
-  };
-
-  const handleKeyboardEvents = ({ key }: KeyboardEvent) => {
-    DRUMKIT_PRESET.forEach((value, soundName) => {
-      if (key == value.key) {
-        playSound(soundName);
-      }
-    });
-  };
-
-  const handlePlayMouseDown = (soundName: string) => () => playSound(soundName);
-
-  const handleGainChange = (soundName: string) => (value: number) => {
-    if (audioBuffer.current[soundName]) {
-      audioBuffer.current[soundName].gainValue = value;
-    }
-  };
+  const [audioContext, setAudioContext] = useState<AudioContext>();
 
   const renderPads = () => {
     let pads: ReactNode[] = [];
-    for (const [soundName] of DRUMKIT_PRESET) {
+    for (const [audioTitle, audioParams] of DRUMKIT_PRESET) {
       pads.push(
         <Pad
-          key={soundName}
-          onMouseDown={handlePlayMouseDown(soundName)}
-          onGainChange={handleGainChange(soundName)}
-          soundName={soundName}
-          defaultGainValue={audioBuffer.current[soundName]?.gainValue}
+          audioContext={audioContext}
+          audioParams={audioParams}
+          audioTitle={audioTitle}
+          key={audioTitle}
         />
       );
     }
@@ -108,20 +34,10 @@ export default function Drums() {
   };
 
   useEffect(() => {
-    setLoading(true);
-    loadSounds().then(() => {
-      setLoading(false);
-    }).catch(console.error);
-  }, [loadSounds]);
-
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeyboardEvents);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyboardEvents);
-    };
-  });
-
+    if (!audioContext) {
+      setAudioContext(new AudioContext());
+    }
+  }, [audioContext])
 
   return (
     <main
@@ -130,8 +46,7 @@ export default function Drums() {
         className="mb-3 text-transparent max-w-[40ch] leading-relaxed text-3xl sm:text-4xl bg-gradient-to-r from-green-500 via-blue-500 to-purple-500 bg-clip-text">Drum
         machine</h1>
       <div className="container mx-auto px-8 grid grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-4">
-        {loading && <p>Loading...</p>}
-        {!loading && renderPads()}
+        {renderPads()}
       </div>
     </main>
   );
